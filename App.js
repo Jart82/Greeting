01b2694ba21661957db7727a5cdc045843acd867
Jart82 <71968782+Jart82@users.noreply.github.com>
2023-09-09 20:48:12 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
+
